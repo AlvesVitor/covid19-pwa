73cd9e030e538d07c384907477ccf48a021c423c
AlvesVitor <01vitoralves@gmail.com>
2022-05-25 14:10:33 -0300

    Update global-style.js

--- src/commons/styles/global-style.js
+++ src/commons/styles/global-style.js
@@ -9,7 +9,7 @@ const globalStyle = createGlobalStyle`
     line-height: normal;
     .MuiPaper-elevation1{
     box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
-    background-color: #5d1f1fb5;
+    background-color: #252626b5;
   }
   .MuiMenu-paper{
     background-color: #000;
